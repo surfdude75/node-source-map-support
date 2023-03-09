24ce420fb9ed86fdcdf7d069cd97ffa142f13aca
Rafael Sobral <rafaelsobral@pm.me>
2023-03-08 21:37:19 -0300

    Fix coffeescript
    From https://github.com/danielbayley/node-source-map-support/tree/patch-1

--- source-map-support.js
+++ source-map-support.js
@@ -147,6 +147,7 @@ function retrieveSourceMapURL(source) {
 
   // Get the URL of the source map
   fileData = retrieveFile(source);
+  if (source.endsWith('node_modules/coffeescript/lib/coffeescript/coffeescript.js')) return null;
   var re = /(?:\/\/[@#][\s]*sourceMappingURL=([^\s'"]+)[\s]*$)|(?:\/\*[@#][\s]*sourceMappingURL=([^\s*'"]+)[\s]*(?:\*\/)[\s]*$)/mg;
   // Keep executing the search to find the *last* sourceMappingURL to avoid
   // picking up sourceMappingURLs from comments, strings, etc.
